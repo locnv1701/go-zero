ff68ad290556a6c3b9815599f85c7c481fd5224f
Nguyen Van Loc <langgo16012001@gmail.com>
2023-11-01 14:58:49 +0700

    first commit

---
+++ greet/internal/logic/login/registerLogic.go
@@ -0,0 +1,34 @@
+package login
+
+import (
+	"context"
+
+	"greet/internal/svc"
+	"greet/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type RegisterLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewRegisterLogic(ctx context.Context, svcCtx *svc.ServiceContext) *RegisterLogic {
+	return &RegisterLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *RegisterLogic) Register(req *types.RegisterReq) (resp *types.Response, err error) {
+	// todo: add your logic here and delete this line
+
+	resp = &types.Response{
+		Message: "register",
+	}
+
+	return
+}
